e267985edec9988c9f07bf478a7dbb30ed7e4164
Wesley Kambale <spartanwk@gmail.com>
2022-09-12 13:50:27 +0300

    Create intern.py

---
+++ intern.py
@@ -0,0 +1,37 @@
+a = 3
+b = 4.8
+
+# Add a & b
+c = a + b
+
+# Print sum of a & b
+print(c)
+
+age = int(input("Age: "))
+
+print(age)
+
+internList = ["John", "Ritah", "Shaban", "Bonitah"]
+
+print(internList)
+
+internList.append("Marion")
+
+print(internList)
+
+print(internList[3])
+
+del internList[3]
+
+print(internList)
+
+internList.append("98")
+
+print(internList)
+
+internTuple = ("John", "Ritah", "Shaban", "Bonitah", 778)
+
+internTuple2 = (23, 46, 85)
+
+print(internTuple + internTuple2)
+
