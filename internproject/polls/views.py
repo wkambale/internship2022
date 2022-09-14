ef972e0965834853ea186c295969a82fc416bf41
Wesley Kambale <spartanwk@gmail.com>
2022-09-14 18:31:54 +0300

    update intern project

--- internproject/polls/views.py
+++ internproject/polls/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
 
+def detail(request, question_id):
+	return HttpResponse("Question %s." % question_id)
 
-# Create your views here.
+def results(request, question_id):
+	response = "Result %s."
+	return HttpResponse(reponse % question_id)
 
-# funcName(param1, param2):
+def vote(request, question_id):
+	return HttpResponse("Vote for Question %s." %question_id)
 
 def index(request):
-	return HttpResponse("Hello, World")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	template = loader.get_template('polls/index.html')
+	context = {
+		'latest_question_list': latest_question_list,
+	}
+	return HttpResponse(template.render(context, request))
