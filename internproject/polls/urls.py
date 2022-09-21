ba379f4bf9df075c7d044f17f54a0185e0034339
Wesley Kambale <spartanwk@gmail.com>
2022-09-21 15:55:50 +0300

    Update Polls App

--- internproject/polls/urls.py
+++ internproject/polls/urls.py
@@ -2,9 +2,15 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
-	path('', views.index, name='index'),
-	path('<int:question_id>/', views.detail, name='detail'),
-	path('<int:question_id>/results/', views.results, name='results'),
+	# 127.0.0.1:8000/polls/
+	path('', views.IndexView.as_view(), name='index'),
+	# 127.0.0.1:8000/polls/1/
+	path('<int:question_id>/', views.DetailView.as_view(), name='detail'),
+	# 127.0.0.1:8000/polls/1/results/
+	path('<int:question_id>/results/', views.ResultsView.as_view(), name='results'),
+	# 127.0.0.1:8000/polls/1/vote/
 	path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
